cfadef2bba79ec8749cee65983e739c141b0c466
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-08 12:42:38 -0400

    Initial commit: Created python file and added docstring header

---
+++ hybrid_calculator.py
@@ -0,0 +1,8 @@
+"""
+Program name: Hybrid Training & pace Calculator
+Author: Dakota Nagy
+Date: June 8, 2026
+Purpose: A terminal-based utility to calculate barbell percentages
+        for strength training and endurance pacing splits.
+Chapters: 1-7 - Python Basics, Control Flow, Functions, Data Structures, OOP, Modules, File I/O
+"""
